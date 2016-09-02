7b0ed9ab537574b2e21a6e511b01346310e66e95
deangao <gaowenhui2012@gmail.com>
2016-09-02 21:22:45 +0800

    更新框架

---
+++ netshadow/netshadow/spiders/netshadow.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+@version: v1.0.0
+@author: deangao 
+@license: Apache Licence 
+@contact: gaowenhui2012@gmail.com
+@site: www.iwhgao.com
+@file: netshadow.py
+@time: 2016/9/2 21:21
+"""
+
+import scrapy
+
+class NetshadowSpider(scrapy.spiders.Spider):
+    name = "netshadow"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
