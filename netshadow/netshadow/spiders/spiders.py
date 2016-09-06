ea137fc60b61a8ac13d8f0047cdc8a545c8c0386
deangao <gaowenhui2012@gmail.com>
2016-09-06 22:56:47 +0800

    init

--- netshadow/netshadow/spiders/spiders.py
+++ netshadow/netshadow/spiders/spiders.py
@@ -12,13 +12,13 @@
 """
 
 import scrapy
+from scrapy import Selector
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from netshadow.items import NetshadowItem
 
 
 class NetshadowSpider(CrawlSpider):
-    def __init__(self, *a, **kw):
-        super(NetshadowSpider, self).__init__(*a, **kw)
 
     name = "netshadow"
     allowed_domains = ["qq.com"]
@@ -29,10 +29,10 @@ class NetshadowSpider(CrawlSpider):
     rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
-        Rule(LinkExtractor(allow=('[0-9]+/[0-9]+/\.htm$',), deny=('subsection\.php',))),
+        Rule(LinkExtractor(allow=('\d+\.htm$',), deny=('subsection\.php',))),
 
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
-        Rule(LinkExtractor(allow=('[0-9]+/[0-9]+/\.htm$',)), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=('\d+\.htm$',)), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
@@ -41,6 +41,8 @@ class NetshadowSpider(CrawlSpider):
         link = str(response.url)
         title = sel.xpath('//div[@class="hd"]/h1/text()').extract()
         content = sel.xpath('//div[@id="Cnt-Main-Article-QQ"]/p/text()').extract()
+
+        print sel,link,title
 
         item['content'] = [n.encode('utf-8') for n in content]
         item['title'] = [n.encode('utf-8') for n in title]
