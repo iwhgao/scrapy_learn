c71da17187b232338df3cf3b3d9f825677d0711c
deangao <gaowenhui2012@gmail.com>
2016-09-06 21:16:46 +0800

    更新框架

--- netshadow/netshadow/spiders/spiders.py
+++ netshadow/netshadow/spiders/spiders.py
@@ -24,7 +24,7 @@ class NetshadowSpider(CrawlSpider):
     name = "netshadow"
     allowed_domains = ["qq.com"]
     start_urls = [
-        "http://www.qq.com/",
+        "http://news.qq.com/a/20160905/052006.htm",
     ]
 
     #rules = [
