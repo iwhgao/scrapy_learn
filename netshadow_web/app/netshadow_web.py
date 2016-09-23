77a2e11e823aacfa5dff65f361d477409fb048da
Wenhui Gao <gaowenhui2012@gmail.com>
2016-09-23 22:19:40 +0800

    更新框架

--- netshadow_web/app/netshadow_web.py
+++ netshadow_web/app/netshadow_web.py
@@ -1,23 +1,72 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+
 from flask import *
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'some_secret'
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+	if not session.get('logged_in'):
+		return redirect(url_for('login'))
+	return render_template('404.html'), 404
+
+
+@app.route("/validate_login", methods=['POST', 'GET'])
+def validate_login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin@163.com':
+			error = 'Invalid username'
+			return redirect(url_for('login'))
+		elif request.form['password'] != 'admin':
+			error = 'Invalid password'
+			return redirect(url_for('login'))
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('index'))
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
-			error = "sorry"
-		else:
-			return redirect(url_for('index'))
-	return render_template('login.html', error=error)
+	# 如果已经登录则直接跳转到主页
+	if session.get('logged_in'):
+		return redirect(url_for('index'))
+
+	return render_template('login.html')
 
 
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	return redirect(url_for('login'))
+
+
+@app.route("/")
 @app.route("/index")
 def index():
+	if not session.get('logged_in'):
+		return redirect(url_for('login'))
+
 	return render_template('index.html')
 
 
+@app.route("/showdata")
+def show_data():
+	if not session.get('logged_in'):
+		return redirect(url_for('login'))
+
+	import MySQLdb
+	conn = MySQLdb.connect(host='localhost', user='root', passwd='')
+	conn.select_db('opensns');
+	cursor = conn.cursor()
+	cursor.execute("select * from ocenter_config limit 20")
+	data = cursor.fetchone()
+	cursor.close()
+	conn.close()
+	print data
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
--- netshadow_web/config.py
+++ netshadow_web/config.py
@@ -8,18 +8,6 @@ class Config:
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    FLASKY_POSTS_PER_PAGE = 20
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
-    FLASKY_SLOW_DB_QUERY_TIME=0.5
 
     @staticmethod
     def init_app(app):
@@ -47,25 +35,6 @@ class ProductionConfig(Config):
     def init_app(cls, app):
         Config.init_app(app)
 
-        # email errors to the administrators
-        import logging
-        from logging.handlers import SMTPHandler
-        credentials = None
-        secure = None
-        if getattr(cls, 'MAIL_USERNAME', None) is not None:
-            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_TLS', None):
-                secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.FLASKY_MAIL_SENDER,
-            toaddrs=[cls.FLASKY_ADMIN],
-            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
-            credentials=credentials,
-            secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
 
 class HerokuConfig(ProductionConfig):
     SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
@@ -78,29 +47,15 @@ class HerokuConfig(ProductionConfig):
         from werkzeug.contrib.fixers import ProxyFix
         app.wsgi_app = ProxyFix(app.wsgi_app)
 
-        # log to stderr
-        import logging
-        from logging import StreamHandler
-        file_handler = StreamHandler()
-        file_handler.setLevel(logging.WARNING)
-        app.logger.addHandler(file_handler)
-
 
 class UnixConfig(ProductionConfig):
     @classmethod
     def init_app(cls, app):
         ProductionConfig.init_app(app)
 
-        # log to syslog
-        import logging
-        from logging.handlers import SysLogHandler
-        syslog_handler = SysLogHandler()
-        syslog_handler.setLevel(logging.WARNING)
-        app.logger.addHandler(syslog_handler)
-
 
 config = {
-    'FLASK_ADMIN': 'gaowenhui2009@aly.com',
+    'FLASK_ADMIN': 'gaowenhui2009@aliyun.com',
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
